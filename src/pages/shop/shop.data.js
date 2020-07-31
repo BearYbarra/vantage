eecf0d59ee8487fb8358111cf81fa4d5829e5625
Jay Ybarra <jayybarra@Jays-MBP.attlocal.net>
2020-07-31 09:32:47 -0500

    update the app with a shop page, collection preview and collection item components

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,231 @@
+const SHOP_DATA = [
+    {
+      id: 1,
+      title: 'Architecture',
+      routeName: 'architecture',
+      items: [
+        {
+          id: 1,
+          name: 'Bauhaus Archive',
+          imageUrl: 'https://i.ibb.co/m5nDt3p/bauhaus-archive.jpg',
+          price: 50
+        },
+        {
+          id: 2,
+          name: 'Burg Eltz',
+          imageUrl: 'https://i.ibb.co/02FZtjW/burg-eltz.jpg',
+          price: 50
+        },
+        {
+          id: 3,
+          name: 'Burj Khalifa',
+          imageUrl: 'https://i.ibb.co/j80j0bQ/burj-khalifa.jpg',
+          price: 50
+        },
+        {
+          id: 4,
+          name: 'Chichén-Itzá',
+          imageUrl: 'https://i.ibb.co/X2RDDqV/Chiche-n-Itza.jpg',
+          price: 50
+        },
+        {
+          id: 5,
+          name: 'Empire State Building',
+          imageUrl: 'https://i.ibb.co/b1kpFCD/empire-state-building.jpg',
+          price: 50
+        },
+        {
+          id: 6,
+          name: 'Hallgrímskirkja',
+          imageUrl: 'https://i.ibb.co/MC1Jpmv/Hallgri-mskirkja.jpg',
+          price: 50
+        },
+        {
+          id: 7,
+          name: 'Pagoda',
+          imageUrl: 'https://i.ibb.co/X5XwxB2/pagoda.jpg',
+          price: 50
+        },
+        {
+          id: 8,
+          name: 'St. Basil\'s Cathedral',
+          imageUrl: 'https://i.ibb.co/Dw1sYZ4/st-basils.jpg',
+          price: 50
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: 'Nature',
+      routeName: 'nature',
+      items: [
+        {
+          id: 1,
+          name: 'Dolomites',
+          imageUrl: 'https://i.ibb.co/SK0y3J9/dolomites.jpg',
+          price: 51
+        },
+        {
+          id: 2,
+          name: 'Grand Canyon',
+          imageUrl: 'https://i.ibb.co/ZzWWqq7/grand-canyon.jpg',
+          price: 51
+        },
+        {
+          id: 3,
+          name: 'Northern Lights',
+          imageUrl: 'https://i.ibb.co/z8yJmsq/northern-lights.jpg',
+          price: 51
+        },
+        {
+          id: 4,
+          name: 'Ocean',
+          imageUrl: 'https://i.ibb.co/JchXcTy/ocean.jpg',
+          price: 51
+        },
+        {
+          id: 5,
+          name: 'Sahara',
+          imageUrl: 'https://i.ibb.co/JmpYw5X/sahara.jpg',
+          price: 51
+        },
+        {
+          id: 6,
+          name: 'Yosemite',
+          imageUrl: 'https://i.ibb.co/Gp4F3k9/yosemite.jpg',
+          price: 51
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: 'Neon',
+      routeName: 'neon',
+      items: [
+        {
+          id: 1,
+          name: 'Girls Just Wanna Have Fun',
+          imageUrl: 'https://i.ibb.co/TPfbwzn/girls.jpg',
+          price: 52
+        },
+        {
+          id: 2,
+          name: 'Hi',
+          imageUrl: 'https://i.ibb.co/F4QpKZw/hi.jpg',
+          price: 52
+        },
+        {
+          id: 3,
+          name: 'Ice Cream',
+          imageUrl: 'https://i.ibb.co/yY649Sy/ice-cream.jpg',
+          price: 52
+        },
+        {
+          id: 4,
+          name: 'All We Have Is Now',
+          imageUrl: 'https://i.ibb.co/c6WY2F8/now.jpg',
+          price: 52
+        },
+        {
+          id: 5,
+          name: 'Peace',
+          imageUrl: 'https://i.ibb.co/sQGyx0M/peace.jpg',
+          price: 52
+        },
+        {
+          id: 6,
+          name: 'Pray',
+          imageUrl: 'https://i.ibb.co/HgMkp6L/pray.jpg',
+          price: 52
+        },
+        {
+          id: 7,
+          name: 'Slow Down',
+          imageUrl: 'https://i.ibb.co/hFvmPJQ/slow-down.jpg',
+          price: 52
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: 'Sculpture',
+      routeName: 'sculpture',
+      items: [
+        {
+          id: 1,
+          name: 'David',
+          imageUrl: 'https://i.ibb.co/nzN0BQj/david.jpg',
+          price: 53
+        },
+        {
+          id: 2,
+          name: 'Ecce Homo',
+          imageUrl: 'https://i.ibb.co/fD1Cdnk/ecce-homo.jpg',
+          price: 53
+        },
+        {
+          id: 3,
+          name: 'Lincoln Memorial',
+          imageUrl: 'https://i.ibb.co/Vwx1MYr/lincoln-memorial.jpg',
+          price: 53
+        },
+        {
+          id: 4,
+          name: 'Statue of Liberty',
+          imageUrl: 'https://i.ibb.co/gPmcMch/statue-of-liberty.jpg',
+          price: 53
+        },
+        {
+          id: 5,
+          name: 'Venus de Milo',
+          imageUrl: 'https://i.ibb.co/Wn7LsSM/venus-de-milo.jpg',
+          price: 53
+        }
+      ]
+    },
+    {
+      id: 5,
+      title: 'Still Life',
+      routeName: 'still-life',
+      items: [
+        {
+          id: 1,
+          name: 'Apples',
+          imageUrl: 'https://i.ibb.co/Jq4NYNY/apples.jpg',
+          price: 54
+        },
+        {
+          id: 2,
+          name: 'Balloons',
+          imageUrl: 'https://i.ibb.co/Vgx8jcV/balloons.jpg',
+          price: 54
+        },
+        {
+          id: 3,
+          name: 'Honey and Clementines',
+          imageUrl: 'https://i.ibb.co/59TRdDg/honey-clementines.jpg',
+          price: 54
+        },
+        {
+          id: 4,
+          name: 'Milk',
+          imageUrl: 'https://i.ibb.co/bRbf8Vt/milk.jpg',
+          price: 54
+        },
+        {
+          id: 5,
+          name: 'Olive Oil',
+          imageUrl: 'https://i.ibb.co/Y7xjRNJ/olive-oil.jpg',
+          price: 54
+        },
+        {
+          id: 6,
+          name: 'Rose',
+          imageUrl: 'https://i.ibb.co/J2yZCCH/rose.jpg',
+          price: 54
+        }
+      ]
+    }
+  ]
+
+  export default SHOP_DATA;
